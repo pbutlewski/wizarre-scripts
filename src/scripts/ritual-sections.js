f188b110d4475bd76206a47bce691365c5f3414e
Piotrek Butlewski <piotrek@butlewski.me>
2023-03-01 13:59:56 +0000

    Update main.js and ritual-sections.js

--- src/scripts/ritual-sections.js
+++ src/scripts/ritual-sections.js
@@ -4,25 +4,25 @@ export function ritualSections() {
   const scroller = scrollama();
   const scroller1 = scrollama();
 
-  let ritualBoxes = document.querySelectorAll('.text-box.is--summoning-box');
-  console.log(ritualBoxes);
-  let ritualImages = document.querySelectorAll('.ritual-image');
-  // eslint-disable-next-line
-  ritualBoxes = [...ritualBoxes].map((box) => {
+  const ritualBoxes = document.querySelectorAll('.text-box.is--summoning-box');
+  const ritualImages = document.querySelectorAll('.ritual-image');
+  const ritualHeadline = document.querySelector('.play-info-section');
+  [...ritualBoxes].forEach((box) => {
     box.classList.contains('is--active')
       ? box.classList.remove('is--active')
       : null;
   });
-  console.log(ritualBoxes);
-  // eslint-disable-next-line
-  ritualImages = [...ritualImages].map((image) => {
+  [...ritualImages].forEach((image) => {
     image.classList.contains('is--active')
       ? image.classList.remove('is--active')
       : null;
   });
+  ritualHeadline.classList.contains('is--active')
+    ? ritualHeadline.classList.remove('is--active')
+    : null;
   scroller
     .setup({
-      step: '.ritual-step',
+      step: '#ritual-animation-start',
       offset: 0.5,
       progress: false,
       debug: false,
@@ -31,27 +31,36 @@ export function ritualSections() {
     .onStepEnter((response) => {
       // { element, index, direction }
       if (response.direction === 'down') {
-        document
-          .querySelectorAll('.text-box.is--summoning-box').forEach((element) => element.classList.add('is--active'));
-          document
-          .querySelectorAll('.ritual-image').forEach((element) => element.classList.add('is--active'));
+        [...ritualBoxes].forEach((element) =>
+          element.classList.add('is--active')
+        );
+        ritualHeadline.classList.add('is--active');
+      } else {
+        [...ritualBoxes].forEach((element) =>
+          element.classList.remove('is--active')
+        );
+        ritualHeadline.classList.remove('is--active');
       }
     });
-      scroller1
+
+  scroller1
     .setup({
-      step: '#ritual',
+      step: '#ritual-animation-middle',
       offset: 0.5,
       progress: false,
       debug: false,
       threshold: 2,
     })
-    .onStepExit((response) => {
+    .onStepEnter((response) => {
       // { element, index, direction }
-      if (response.direction === 'up') {
-        document
-          .querySelectorAll('.text-box.is--summoning-box').forEach((element) => element.classList.remove('is--active'));
-          document
-          .querySelectorAll('.ritual-image').forEach((element) => element.classList.remove('is--active'));
+      if (response.direction === 'down') {
+        [...ritualImages].forEach((element) =>
+          element.classList.add('is--active')
+        );
+      } else {
+        [...ritualImages].forEach((element) =>
+          element.classList.remove('is--active')
+        );
       }
     });
 }
